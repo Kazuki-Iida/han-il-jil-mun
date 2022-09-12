35b58ea343634b77434494e096f4efe8a46eb688
Kazuki-Iida <kazuki083177@gmail.com>
2022-09-12 10:32:13 +0900

    Answerのmodelとcontrollerの中身を軽く書いた

--- app/Answer.php
+++ app/Answer.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-    //
+    public function index(Answer $answer)
+    {
+        return $answer->get();
+    }
+    
+    public function getPaginateByLimit(int $limit_count = 10)
+    {
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
+    
+    protected $fillable = [
+        'body',
+        'user_id',
+    ];
 }
--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// use Illuminate\Http\AnswerRequest;
+use App\Question;
 
 class AnswerController extends Controller
 {
-    //
+    public function index(Answer $answer)
+    {
+        return view('answers/index')->with(['answers' => $answer->getPaginateByLimit()]);  
+    }
+    
+    public function show(Answer $answer)
+    {
+        return view('answers/show')->with(['answer' => $answer]);
+    }
+    
+    public function create(Category $category)
+    {
+        return view('answers/create')->with(['categories' => $category->get()]);;
+    }
+    
+    public function store(Answer $answer, AnswerRequest $request) 
+    {
+        $input = $request['answer'];
+        $answer->fill($input)->save();
+        return redirect('/answers/' . $answer->id);
+    }
+
 }
