57fd33dec758d0588e0e5579b5f6c4dd6e59c460
Kazuki-Iida <kazuki083177@gmail.com>
2022-09-29 22:42:35 +0900

    いいねのような機能を付け加えた後のindexページの体裁を整えた

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -14,29 +14,34 @@
                 <div class="question-index-inner bg-white col-sm-11 col-md-7 mx-auto">
                     [<a href='/questions/create'>create</a>]
                     <div class='questions'>
-                        <div class="card-wrapper mb-4 mr-2 ml-2">
+                        <div class="card-wrapper mb-4 mr-2 ml-2 border-top border-success">
                             @foreach ($questions as $question)
-                                <div class="question card mt-3 border-success" id="{{ $i }}">
-                                    <div class='question-inner card-body'>
-                                        <div class="question-header row">
-                                            <h2 class="title card-titile col-7">
-                                                <a href="/questions/{{ $question->id }}">{{ Str::limit( $question->title, 40) }}</a>
+                                <div class="question row mt-0 border-success border-bottom" id="{{ $i }}">
+                                    <div class="index-user-image col-2 pt-4 pl-3 pr-0">
+                                        <a href="/users/{{ $question->user_id }}"><img src="{{ $question->user->profile_image }}" alt="Contact Person" class="img-fuild rounded-circle" width="50" height="50"></a>
+                                    </div>
+                                    <div class='question-inner card-body col-10 pl-0'>
+                                        <div class="question-user">
+                                            <a class="user-name" href="/users/{{ $question->user_id }}">{{ Str::limit( $question->user->name,40) }}</a>
+                                        </div>
+                                        <div class="question-header">
+                                            <h2 class="title card-titile">
+                                                <a class="card-titile" href="/questions/{{ $question->id }}">{{ Str::limit( $question->title, 40) }}</a>
                                             </h2>
-                                            <div class="question-user col-5">
-                                                <a href="/users/{{ $question->user_id }}"><img src="{{ $question->user->profile_image }}" alt="Contact Person" class="img-fuild rounded-circle" width="40" height="40">{{ Str::limit( $question->user->name,40) }}</a>
-                                            </div>
                                         </div>
                                         <a href="/questions/{{ $question->id }}"><p class='body card-text mt-3 mb-4'>{{ $question->body }}</p></a>
-                                        <div>
-                                            @if($question->is_liked_by_auth_user())
-                                                <a href="{{ route('question.unlike', ['question_id' => $question->id, 'id' => $i]) }}" class="btn btn-success btn-sm">いいね<span class="badge">{{ $question->likes->count() }}</span></a>
-                                            @else
-                                                <a href="{{ route('question.like', ['question_id' => $question->id, 'id' => $i]) }}" class="btn btn-secondary btn-sm">いいね<span class="badge">{{ $question->likes->count() }}</span></a>
-                                            @endif
-                                            {{ $question->likes->count() }}
+                                        <div class="row">
+                                            <div class="col-3">
+                                                @if($question->is_liked_by_auth_user())
+                                                    <a href="{{ route('question.unlike', ['question_id' => $question->id, 'id' => $i]) }}" class="btn btn-success btn-sm">Good<span class="badge">{{ $question->likes->count() }}</span></a>
+                                                @else
+                                                    <a href="{{ route('question.like', ['question_id' => $question->id, 'id' => $i]) }}" class="btn btn-secondary btn-sm">Good<span class="badge">{{ $question->likes->count() }}</span></a>
+                                                @endif
+                                            </div>
+                                            <p class="col-3"><a class="card-link" href="/categories/{{ $question->category->id }}">{{ $question->category->name }}</a></p>
+                                            <p class="col-3"><a class="card-link" href="/countries/{{ $question->country->id }}">{{ $question->country->name }}</a></p>
+                                            <div class="answers-count col-3"><p>回答数：{{ $question->answers->count() }}</p></div>
                                         </div>
-                                        <p>カテゴリー：<a class="card-link" href="/categories/{{ $question->category->id }}">{{ $question->category->name }}</a></p>
-                                        <p>about:<a class="card-link" href="/countries/{{ $question->country->id }}">{{ $question->country->name }}</a></p>
                                     </div>
                                 </div>
                                 <p style="display:none">{{ $i++ }}</p>
