8e17d99fde6b062afa9ae25fc6a353e8c6455048
Kazuki-Iida <kazuki083177@gmail.com>
2022-10-13 02:39:45 +0900

    質問表示画面デザイン実装

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -22,31 +22,31 @@
             	}
             }
         </script>
-<!--        <ul class="list">-->
-<!--    <li class="list-item">item1</li>-->
-<!--    <li class="list-item">item2</li>-->
-<!--    <li class="list-item">item3</li>-->
-<!--    <li class="list-item">item4</li>-->
-<!--    <li class="list-item">item5</li>-->
-<!--    <li class="list-item">item6</li>-->
-<!--    <li class="list-item">item7</li>-->
-<!--    <li class="list-item">item8</li>-->
-<!--    <li class="list-item">item9</li>-->
-<!--    <li class="list-item">item10</li>-->
-<!--    <li class="list-item">item11</li>-->
-<!--    <li class="list-item">item12</li>-->
-<!--    <li class="list-item">item13</li>-->
-<!--    <li class="list-item">item14</li>-->
-<!--    <li class="list-item">item15</li>-->
-<!--    <li class="list-item">item16</li>-->
-<!--    <li class="list-item">item17</li>-->
-<!--    <li class="list-item">item18</li>-->
-<!--    <li class="list-item">item19</li>-->
-<!--    <li class="list-item">item20</li>-->
-<!--</ul>-->
-<!--<div class="list-btn">-->
-<!--    <button>もっと見る</button>-->
-<!--</div>-->
+{{--        <ul class="list">
+    <li class="list-item">item1</li>
+    <li class="list-item">item2</li>
+    <li class="list-item">item3</li>
+    <li class="list-item">item4</li>
+    <li class="list-item">item5</li>
+    <li class="list-item">item6</li>
+    <li class="list-item">item7</li>
+    <li class="list-item">item8</li>
+    <li class="list-item">item9</li>
+    <li class="list-item">item10</li>
+    <li class="list-item">item11</li>
+    <li class="list-item">item12</li>
+    <li class="list-item">item13</li>
+    <li class="list-item">item14</li>
+    <li class="list-item">item15</li>
+    <li class="list-item">item16</li>
+    <li class="list-item">item17</li>
+    <li class="list-item">item18</li>
+    <li class="list-item">item19</li>
+    <li class="list-item">item20</li>
+</ul>
+<div class="list-btn">
+    <button>もっと見る</button>
+</div>  --}}
         <div class="container">
             <div class="row">
                 <div class="question-show-inner col-12 col-lg-7 bg-white mx-auto border-bottom  border-secondary rounded">
@@ -194,9 +194,9 @@
                                                 <a href="{{ route('answer.like', ['answer_id' => $answer->id]) }}" class="good-btn btn btn-secondary btn-sm text-nowrap"><i class="far fa-thumbs-up"></i><span class="badge">{{ $answer->likes->count() }}</span></a>
                                             @endif
                                         </div>
-                                        <div class="col-space col-3 d-flex align-items-center">
+                                        <div class="col-space col-2 d-flex align-items-center">
                                         </div>
-                                        <div class="to-comment-page col-6 text-nowrap text-center d-flex align-items-center">
+                                        <div class="to-comment-page col-7 text-nowrap text-center d-flex align-items-center">
                                             <a class="btn btn-success btn-lg rounded-pill text-nowrap m-auto py-1" href="/comments/{{ $answer->id }}/create">コメントする&ensp;<i class="far fa-comments"></i></a>
                                         </div>
                                     </div>
@@ -206,77 +206,79 @@
                             <?php $i = 1; ?>
                             <!--画像表示用-->
                             <?php $k = 0; ?>
-                            @foreach ($answer->comments as $comment)
-                                <div class="comment row mt-0 border-secondary border-bottom">
-                                    <div class="index-user-image col-2 pt-4 pl-3 pr-0">
-                                        <a href="/users/{{ $comment->user_id }}"><img src="{{ $comment->user->profile_image }}" alt="Contact Person" class="img-fuild rounded-circle" width=50 height=50></a>
-                                    </div>
-                                    <div class='comment-inner card-body col-10 pl-0 pb-2'>
-                                        <div class="comment-card-header">
-                                            @auth
-                                                <div class="dropdown float-right"> 
-                                                    <button id="btnOpenMenu" class="detail-btn"  
-                                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                        <i class="fas fa-ellipsis-h"></i>
-                                                    </button>
-                                                    <div class="dropdown-menu pb-0" aria-labelledby="btnOpenMenu">
-                                                        @if($comment->user->id == Auth::user()->id)
-                                                            <form action="/comments/{{ $comment->id }}/edit" method="GET">
-                                                                @csrf
-                                                                <button class="dropdown-item" type="submit">コメントを編集する</button>
-                                                            </form>
-                                                            <form action="/comments/{{ $comment->id }}" method="POST">
-                                                                @csrf
-                                                                @method('DELETE')
-                                                                <button class="dropdown-item" type="submit">コメントを削除する</button>
-                                                            </form>
-                                                        @else
-                                                            @if($comment->is_reported_by_auth_user())
-                                                                <a href="{{ route('comment.unreport', ['comment_id' => $comment->id]) }}" class="dropdown-item">通報済み</a>
+                            <div class="comments">
+                                @foreach ($answer->comments as $comment)
+                                    <div class="comment row mt-0 border-secondary border-bottom">
+                                        <div class="index-user-image col-2 pt-4 pl-3 pr-0">
+                                            <a href="/users/{{ $comment->user_id }}"><img src="{{ $comment->user->profile_image }}" alt="Contact Person" class="img-fuild rounded-circle" width=50 height=50></a>
+                                        </div>
+                                        <div class='comment-inner card-body col-10 pl-0 pb-2'>
+                                            <div class="comment-card-header">
+                                                @auth
+                                                    <div class="dropdown float-right"> 
+                                                        <button id="btnOpenMenu" class="detail-btn"  
+                                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                            <i class="fas fa-ellipsis-h"></i>
+                                                        </button>
+                                                        <div class="dropdown-menu pb-0" aria-labelledby="btnOpenMenu">
+                                                            @if($comment->user->id == Auth::user()->id)
+                                                                <form action="/comments/{{ $comment->id }}/edit" method="GET">
+                                                                    @csrf
+                                                                    <button class="dropdown-item" type="submit">コメントを編集する</button>
+                                                                </form>
+                                                                <form action="/comments/{{ $comment->id }}" method="POST">
+                                                                    @csrf
+                                                                    @method('DELETE')
+                                                                    <button class="dropdown-item" type="submit">コメントを削除する</button>
+                                                                </form>
                                                             @else
-                                                                <a href="{{ route('comment.report', ['comment_id' => $comment->id]) }}" class="dropdown-item">通報する</a>
+                                                                @if($comment->is_reported_by_auth_user())
+                                                                    <a href="{{ route('comment.unreport', ['comment_id' => $comment->id]) }}" class="dropdown-item">通報済み</a>
+                                                                @else
+                                                                    <a href="{{ route('comment.report', ['comment_id' => $comment->id]) }}" class="dropdown-item">通報する</a>
+                                                                @endif
                                                             @endif
-                                                        @endif
+                                                        </div>
+                                                    </div>
+                                                @endauth
+                                                <div class="comment-user pt-2 pl-sm-0 pl-3">
+                                                    <a class="user-name pl-2 pl-sm-0" href="/users/{{ $comment->user_id }}">{{ Str::limit( $comment->user->name,40) }}</a>
+                                                </div>
+                                            </div>
+                                            <div class="comment-body">
+                                                <p class='body card-text mt-3 mb-4'>{{ $comment->body }}</p>
+                                                 <div class="images row px-3 pl-sm-3 pl-3 pr-sm-5 pr-3">
+                                                    @foreach($comment->comment_images as $comment_image)
+                                                        <a href="{{ $comment_image->image }}" data-lightbox="{{ $k }}" data-alt="コメントに関する画像" class="col-6 p-0 pr-1">
+                                                            <img src="{{ $comment_image->image }}" alt="comment images" class="image img-fuild mb-1 w-100 rounded">
+                                                        </a>
+                                                    @endforeach
+                                                </div>
+                                                <?php $k--;  ?>
+                                            </div>
+                                            <div class="row mb-1 ml-0">
+                                                <div class="col-space col-4 m-0 p-0 text-left text-nowrap">
+                                                </div>
+                                                <div class="created_at col-8 p-0 pr-3 text-right text-nowrap">
+                                                    <p class="mb-0">({{ $comment->created_at->format('Y/m/d-G:m:s') }})</p>
+                                                </div>
+                                            </div>
+                                            @if($i == $answer->comments->count())
+                                                <div class="row comment-card-footer w-100 pt-0 px-2 pb-2">
+                                                    <div class="col-space col-3 pl-0 d-flex align-items-center">
+                                                    </div>
+                                                    <div class="col-space col-2 d-flex align-items-center">
+                                                    </div>
+                                                    <div class="to-comment-page col-7 text-nowrap text-center d-flex align-items-center">
+                                                        <a class="btn btn-success btn-lg rounded-pill text-nowrap m-auto py-1" href="/comments/{{ $answer->id }}/create">コメント追加&ensp;<i class="far fa-comments"></i></a>
                                                     </div>
                                                 </div>
-                                            @endauth
-                                            <div class="comment-user pt-2 pl-sm-0 pl-3">
-                                                <a class="user-name pl-2 pl-sm-0" href="/users/{{ $comment->user_id }}">{{ Str::limit( $comment->user->name,40) }}</a>
-                                            </div>
+                                            @endif
                                         </div>
-                                        <div class="comment-body">
-                                            <p class='body card-text mt-3 mb-4'>{{ $comment->body }}</p>
-                                             <div class="images row px-3 pl-sm-3 pl-3 pr-sm-5 pr-3">
-                                                @foreach($comment->comment_images as $comment_image)
-                                                    <a href="{{ $comment_image->image }}" data-lightbox="{{ $k }}" data-alt="コメントに関する画像" class="col-6 p-0 pr-1">
-                                                        <img src="{{ $comment_image->image }}" alt="comment images" class="image img-fuild mb-1 w-100 rounded">
-                                                    </a>
-                                                @endforeach
-                                            </div>
-                                            <?php $k--;  ?>
-                                        </div>
-                                        <div class="row mb-1 ml-0">
-                                            <div class="col-space col-4 m-0 p-0 text-left text-nowrap">
-                                            </div>
-                                            <div class="created_at col-8 p-0 pr-3 text-right text-nowrap">
-                                                <p class="mb-0">({{ $comment->created_at->format('Y/m/d-G:m:s') }})</p>
-                                            </div>
-                                        </div>
-                                        @if($i == $answer->comments->count())
-                                            <div class="row comment-card-footer w-100 pt-0 px-2 pb-2">
-                                                <div class="col-space col-3 pl-0 d-flex align-items-center">
-                                                </div>
-                                                <div class="col-space col-3 d-flex align-items-center">
-                                                </div>
-                                                <div class="to-comment-page col-6 text-nowrap text-center d-flex align-items-center">
-                                                    <a class="btn btn-success btn-lg rounded-pill text-nowrap m-auto py-1" href="/comments/{{ $answer->id }}/create">コメント追加&ensp;<i class="far fa-comments"></i></a>
-                                                </div>
-                                            </div>
-                                        @endif
                                     </div>
-                                </div>
-                                <?php $i++; ?>
-                            @endforeach
+                                    <?php $i++; ?>
+                                @endforeach
+                            </div>
                         @endforeach
                     </div>
                     <div class="footer py-3">
