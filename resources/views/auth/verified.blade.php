8c3066eda8b1551d795074e37f5da60f9535a659
Kazuki-Iida <kazuki083177@gmail.com>
2022-10-18 02:00:36 +0900

    verified.blade.phpの見た目を整えた。

--- resources/views/auth/verified.blade.php
+++ resources/views/auth/verified.blade.php
@@ -6,11 +6,12 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card pt-5">
+            <div class="card mt-5">
                 <div class="card-header">{{ __('You\'ve verified your email address.') }}</div>
 
                 <div class="card-body">
-                    {{ __('Let\'s get started.') }}
+                    {{ __('Let\'s get started.') }}&ensp;&ensp;
+                    <a href="/" class="">トップページへ&ensp;<i class="fas fa-home"></i></a>
                 </div>
             </div>
         </div>
