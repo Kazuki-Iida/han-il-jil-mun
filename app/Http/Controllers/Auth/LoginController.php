82246865d0c2d15b562e827def725bf7ca85e14e
Kazuki-Iida <kazuki083177@gmail.com>
2022-10-18 15:39:15 +0900

    googleでwebサイトを審査中なのでgoogleでログインする機能を一旦隠した

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\User;
 use Socialite;
 
 class LoginController extends Controller
@@ -39,17 +40,32 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
     
-    
-     public function redirectToGoogle()
+    public function redirectToGoogle()
     {
-        // Google へのリダイレクト
         return Socialite::driver('google')->redirect();
     }
 
     public function handleGoogleCallback()
     {
-        // Google 認証後の処理
         $gUser = Socialite::driver('google')->stateless()->user();
-        dd($gUser);
+        // email が合致するユーザーを取得
+        $user = User::where('email', $gUser->email)->first();
+        // 見つからなければ新しくユーザーを作成
+        if ($user == null) {
+            $user = $this->createUserByGoogle($gUser);
+        }
+        // ログイン処理
+        \Auth::login($user, true);
+        return redirect('/home');
+    }
+
+    public function createUserByGoogle($gUser)
+    {
+        $user = User::create([
+            'name'     => $gUser->name,
+            'email'    => $gUser->email,
+            'password' => \Hash::make(uniqid()),
+        ]);
+        return $user;
     }
 }
