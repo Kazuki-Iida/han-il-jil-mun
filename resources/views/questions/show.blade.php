1cb0b0b802144f2be419a72c8bf05ae9a8903c29
Kazuki-Iida <kazuki083177@gmail.com>
2022-10-19 01:18:33 +0900

    タイトルつけたことによるエラーの修理

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,7 +5,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>@section('title', {{ $question->title }})</title>
+        <title>@section('title', '質問を表示')</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="/css/app.css">
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,7 +5,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>@section('title', {{ $user->name }})</title>
+        <title>@section('title', 'ユーザープロフィール')</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="/css/app.css">
